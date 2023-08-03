8c2bac3065d6d105fe301d7d8e90b9559108173f
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-03 12:35:16 +0330

    Add files via upload

---
+++ ConsoleApp42/Class2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp42
+{
+   public class Employee
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public Date BirthDate { get; }
+        public Date HireDate { get; }
+
+        public Employee(string firstName, string lastName, Date birthDate, Date hireDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            HireDate = hireDate;
+        }
+
+        public override string ToString() => $"{LastName},{FirstName}" + $"Hired:{HireDate} Birthday:{BirthDate}";
+        
+            
+        }
+    }
+
---
+++ ConsoleApp42/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var birthday = new Date(7, 24, 1949);
+            var hireDate = new Date(3, 12, 1988);
+            var employee = new Employee("Bob", "Blue", birthday, hireDate);
+
+            Console.WriteLine(employee);
+            Console.ReadLine();
+        }
+    }
+}
